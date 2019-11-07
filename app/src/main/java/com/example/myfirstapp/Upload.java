10f6698545574a1b822ccb7af3d5198e9c40a5ad
JuliusWildenburg <julius.wildenburg@gmail.com>
2019-11-07 22:13:33 +0100

    commitment

---
+++ app/src/main/java/com/example/myfirstapp/Upload.java
@@ -0,0 +1,43 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class Upload extends AppCompatActivity {
+
+    private static final int PICK_IMAGE_REQUEST=1;
+
+    private Button mButtonChooseImage;
+    private Button mButtonUpload;
+    private TextView mTextViewShowUploads;
+    private EditText mEditTextFileName;
+    private ImageView mImageView;
+    private ProgressBar mProgressbar;
+
+    private Uri mImageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        mButtonChooseImage = findViewById(R.id.button_choose_image);
+        mButtonUpload = findViewById(R.id.button_upload);
+        mTextViewShowUploads = findViewById(R.id.text_view_show_uploads);
+        mEditTextFileName = findViewById(R.id.edit_text_file_name);
+        mImageView = findViewById(R.id.image_view);
+        mProgressbar = findViewById(R.id.progress_bar);
+
+        mButtonChooseImage.setOnClickListener();
+
+
+
+    }
+}
