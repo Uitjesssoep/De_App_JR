324a1fd11161f9d43a7280d4612a33237a5f2af7
Robbert <robbertschmeetz@gmail.com>
2019-11-19 15:16:02 +0100

    Commit van Robbert

--- app/src/main/java/com/example/myfirstapp/Text_Post_Viewing_Activity.java
+++ app/src/main/java/com/example/myfirstapp/Text_Post_Viewing_Activity.java
@@ -6,9 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,9 +22,16 @@ import com.google.firebase.database.ValueEventListener;
 public class Text_Post_Viewing_Activity extends AppCompatActivity {
 
 
-    private TextView Title, Content, UserName;
+    private TextView Title, Content, UserName, LikeCount;
     private FirebaseDatabase firebaseDatabase;
-    private String key;
+    private String key, MyUID;
+    private ImageButton Like, Dislike;
+    private boolean Liked = false;
+    private boolean Disliked = false;
+
+    private DatabaseReference DatabaseLike, DatabaseDislike, DatabaseIsItLiked, DatabaseIsItDisliked;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
 
     private void SetupUI() {
@@ -28,8 +39,13 @@ public class Text_Post_Viewing_Activity extends AppCompatActivity {
         Title = findViewById(R.id.tvTitleOfTextPost);
         Content = findViewById(R.id.tvContentOfTextPost);
         UserName = findViewById(R.id.tvUsernameForTextPost);
+        LikeCount = findViewById(R.id.tvLikeCounterForTextPostViewing);
+        Like = findViewById(R.id.ibLikeUpForTextPostViewing);
+        Dislike = findViewById(R.id.ibLikeDownForTextPostViewing);
 
         firebaseDatabase = FirebaseDatabase.getInstance();
+
+        MyUID = firebaseAuth.getCurrentUser().getUid().toString();
 
     }
 
@@ -41,6 +57,8 @@ public class Text_Post_Viewing_Activity extends AppCompatActivity {
         setTitle("Text Post");
 
         SetupUI();
+
+        LikeDislikeCount();
 
         key = getIntent().getExtras().get("Key").toString();
 
@@ -85,6 +103,129 @@ public class Text_Post_Viewing_Activity extends AppCompatActivity {
                 Toast.makeText(Text_Post_Viewing_Activity.this, "Couldn't retrieve data from database", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void LikeDislikeCount() {
+
+        Like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Liked = true;
+
+
+                    DatabaseLike = FirebaseDatabase.getInstance().getReference().child("Likes");
+                    DatabaseLike.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            if(Liked) {
+
+                                if (dataSnapshot.child(key).hasChild(MyUID)) {
+
+                                    DatabaseLike.child(key).child(MyUID).removeValue();
+                                    Liked = false;
+
+                                } else {
+
+                                    DatabaseLike.child(key).child(MyUID).setValue("RandomLike");
+                                    Liked = false;
+
+                                }
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+            }
+        });
+
+        Dislike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Disliked = true;
+
+                    DatabaseDislike = FirebaseDatabase.getInstance().getReference().child("Dislikes");
+                    DatabaseDislike.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            if(Disliked) {
+
+                                if (dataSnapshot.child(key).hasChild(MyUID)) {
+
+                                    DatabaseDislike.child(key).child(MyUID).removeValue();
+                                    Disliked = false;
+
+                                } else {
+
+                                    DatabaseDislike.child(key).child(MyUID).setValue("RandomDislike");
+                                    Disliked = false;
+
+                                }
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+            }
+        });
+
+        DatabaseIsItLiked = FirebaseDatabase.getInstance().getReference().child("Likes");
+        DatabaseIsItLiked.keepSynced(true);
+        DatabaseIsItLiked.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.child(key).hasChild(MyUID)) {
+
+                    Like.setImageResource(R.drawable.pijl_omhoog_geklikt);
+
+                }
+
+                else{
+
+                    Like.setImageResource(R.drawable.pijl_omhoog_neutraal);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        DatabaseIsItDisliked = FirebaseDatabase.getInstance().getReference().child("Dislikes");
+        DatabaseIsItDisliked.keepSynced(true);
+        DatabaseIsItDisliked.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.child(key).hasChild(MyUID)) {
+
+
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 
