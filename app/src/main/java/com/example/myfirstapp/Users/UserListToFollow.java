c210c69ff6fcdc3b18db285430771484700eb2e0
JuliusWildenburgSchool <julius.wildenburg@gmailcom>
2020-01-10 15:19:25 +0100

    Commitment school

--- app/src/main/java/com/example/myfirstapp/Users/UserListToFollow.java
+++ app/src/main/java/com/example/myfirstapp/Users/UserListToFollow.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myfirstapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,16 +36,16 @@ public class UserListToFollow extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabaseUIDlist, firebaseDatabase;
     private ProgressBar ProgressCircle;
     private RecyclerView recyclerView;
-    // private FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
     private RecyclerView.LayoutManager layoutManager;
     private List<Users> list;
     private List<String> UIDlist;
     private UserAdapter adapter;
-    private String UIDlistString, test, UIDlistString2,UIDString;
+    private String UIDlistString, test, UIDlistString2;
 
     private void SetupUI() {
         firebaseDatabase = FirebaseDatabase.getInstance();
-        // firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
         firebaseStorage = FirebaseStorage.getInstance();
         ProgressCircle = findViewById(R.id.progress_circle3);
         recyclerView = findViewById(R.id.recycler_viewUserList);
@@ -59,47 +60,22 @@ public class UserListToFollow extends AppCompatActivity {
 
     }
 
-    private void MakeUIDlist() {
-        databaseReferenceUIDlist.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot dsp2 : dataSnapshot.getChildren()){
-            //        UID MUID = dsp2.getValue(UID.class);
-                    UIDlistString2=dsp2.getKey();
-                    UIDlist.add(UIDlistString2);
-              //  for (DataSnapshot dsp : dataSnapshot.getChildren()) {
-               //     UIDlist.add(String.valueOf(dsp.getValue()));
-                    UIDlistString = UIDlist.toString();
-                    Log.d("UIDLIST", UIDlistString);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-            }
-        });
-    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list_to_follow);
-        SetupUI();
-      //  MakeUIDlist();
+        SetupUI();;
         databaseReference = firebaseDatabase.getReference("users");
 
-        // test = firebaseDatabase.getReference("users").child("7yTA9yX4fiTGw5oHInp7rcPIDHF2").child("userBrithdate").toString();
-        //  Log.d("TESTT", test);
             databaseReference.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                    //     Users users = dataSnapshot.getValue().toString();
                     for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                         Users users = postSnapshot.getValue(Users.class);
                         list.add(users);
-                        //      Log.d("list", UIDlist.toString());
                         Log.d("list", list.toString());
 
                     }
