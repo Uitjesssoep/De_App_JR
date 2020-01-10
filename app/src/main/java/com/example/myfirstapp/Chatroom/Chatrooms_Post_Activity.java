b213c6dc9d2f8892a95fba8adc1ac7b4c1850441
RobbertVanSchool <robbertschmeetz@gmail.com>
2020-01-10 14:45:17 +0100

    van school 2.0 robbert

--- app/src/main/java/com/example/myfirstapp/Chatroom/Chatrooms_Post_Activity.java
+++ app/src/main/java/com/example/myfirstapp/Chatroom/Chatrooms_Post_Activity.java
@@ -3,16 +3,46 @@ package com.example.myfirstapp.Chatroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myfirstapp.R;
 
 public class Chatrooms_Post_Activity extends AppCompatActivity {
 
-
+    EditText Title;
+    Button Post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatrooms__post_);
+
+        SetupUI();
+
+        Post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String TitleString = Title.getText().toString();
+
+                if(TitleString.isEmpty()){
+                    Toast.makeText(Chatrooms_Post_Activity.this, "You must have a title", Toast.LENGTH_LONG);
+                }
+                else{
+
+                }
+
+            }
+        });
+    }
+
+    private void SetupUI() {
+
+        Title = findViewById(R.id.etTitleChatroom);
+        Post = findViewById(R.id.btnPostChatRoom);
+
     }
 }
---
+++ app/src/main/java/com/example/myfirstapp/Chatroom/PostStuffForChat.java
@@ -0,0 +1,19 @@
+package com.example.myfirstapp.Chatroom;
+
+public class PostStuffForChat {
+
+    private String mTitle;
+    private String mUser_name;
+    private String mUID;
+    private String mKey;
+    private String mDate;
+
+    public PostStuffForChat(String Title, String User_name, String UID, String Key, String Date) {
+        mTitle = Title;
+        mUser_name = User_name;
+        mUID = UID;
+        mKey = Key;
+        mDate = Date;
+    }
+
+}
