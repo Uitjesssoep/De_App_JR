9a924c4546427c8f5720ccccaabffb1da30d5b80
RobbertVanSchool <robbertschmeetz@gmail.com>
2020-01-29 14:21:56 +0100

    van school 2.0 robbert

--- app/src/main/java/com/example/myfirstapp/AccountFragment.java
+++ app/src/main/java/com/example/myfirstapp/AccountFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +14,13 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class AccountFragment extends Fragment {
 
@@ -26,7 +35,9 @@ public class AccountFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-
+        
+        AccountInfoLoading();
+        
         TabLayout tabLayout = getView().findViewById(R.id.tab_layout_personal_account);
         TabItem MyPosts = getView().findViewById(R.id.tab_layout_MyPosts);
         TabItem SavedPosts = getView().findViewById(R.id.tab_layout_SavedPosts);
@@ -62,6 +73,37 @@ public class AccountFragment extends Fragment {
         });
 
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+    }
+
+    private void AccountInfoLoading() {
+
+        String MyUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference GetAccountInfo = FirebaseDatabase.getInstance().getReference("users").child(MyUID);
+
+        GetAccountInfo.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                ImageView ProfilePicture = getView().findViewById(R.id.ivProfilePictureAccountInfoViewing);
+                TextView DisplayName = getView().findViewById(R.id.tvDisplayNameMyAccountViewing);
+                TextView UserName = getView().findViewById(R.id.tvUsernameMyAccountViewing);
+
+                String DN = dataSnapshot.child("userFullName").getValue().toString();
+                DisplayName.setText(DN);
+
+                String UN = dataSnapshot.child("userName").getValue().toString();
+                UserName.setText(UN);
+
+                String PF = dataSnapshot.child("profilePicture").getValue().toString();
+                Picasso.get().load(PF).fit().centerCrop().into(ProfilePicture);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
