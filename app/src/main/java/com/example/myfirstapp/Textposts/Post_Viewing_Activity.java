7fefb962d3471de20a72ed7c6b11d9b677238147
Robbert <robbertschmeetz@gmail.com>
2020-02-08 10:54:44 +0100

    Robbert

--- app/src/main/java/com/example/myfirstapp/Textposts/Post_Viewing_Activity.java
+++ app/src/main/java/com/example/myfirstapp/Textposts/Post_Viewing_Activity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
